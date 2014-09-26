d0e8f7ed550385b6a3baade1eca67649aff15fd2
Raj Manchekar <raj.manchekar@lrn.com>
2014-09-26 15:31:31 +0530

    zend project folder structure

--- learn.php
+++ learn.php
@@ -1,3 +1,52 @@
 <?php
     echo "learning Zend framework";
+	
+	echo "folder structure:
+
+		1) There are 3 folder is the root directory
+			- application
+			- library
+			- public
+			
+		2) Inside application folder there are 4 folder and 1 Bootstrap.php file:
+			- models
+			- views
+			- controllers
+			- configs
+			- Bootstrap.php
+			
+		3) Inside configs folder there is application.ini file
+		
+		4) Inside controllers folder there are 2 php files (by default)
+			- IndexController.php
+			- ErrorController.php
+			
+		5) Inside views folder there are 2 folder:
+			- helpers
+			- scripts
+			
+		6) Inside scripts there 3 folder:
+			- error
+			- index
+			
+		7) Inside error folder there is error.phtml file
+		
+		8) Inside index folder there is index.phtml file
+		
+		9) helper folder is empty
+		
+		10) Inside public folder there are 2 files:
+			- .htaccess
+			- index.php
+	
+	";
+	
+	
+	
+	
+	
+	
+	
+	
+	
 ?>
